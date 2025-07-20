3619ee6d54bec326cf361028105b5720593be02a
Ivan Shlyapin <shlyapin@gmail.com>
2025-07-20 16:28:59 +0300

    feat: initial commit

---
+++ bin/dart_async_homework2.dart
@@ -0,0 +1,5 @@
+import 'package:dart_async_homework2/dart_async_homework2.dart' as dart_async_homework2;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_async_homework2.calculate()}!');
+}
---
+++ test/dart_async_homework2_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_async_homework2/dart_async_homework2.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
