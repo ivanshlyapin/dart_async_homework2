3f5f53f6e126ac906f5edf55a047ea477c1a4e36
Ivan Shlyapin <shlyapin@gmail.com>
2025-07-20 16:43:53 +0300

    feat(DART-11): Task 2 done

--- lib/homeworks/lesson_11/hw_futures.dart
+++ lib/homeworks/lesson_11/hw_futures.dart
@@ -3,7 +3,21 @@ Future<String> fetchName() async {
   return 'Іван';
 }
 
+Future<String> fetchAge() async {
+  await Future.delayed(Duration(milliseconds: 1500));
+  return '25';
+}
+
+String getAgeSuffix(int age) {
+  if (age % 10 == 1 && age % 100 != 11) return 'рік';
+  if ([2, 3, 4].contains(age % 10) && !(age % 100 >= 12 && age % 100 <= 14)) return 'роки';
+  return 'років';
+}
+
 void main() async {
   String name = await fetchName();
   print('Мене звати $name');
+  String ageStr = await fetchAge();
+  int age = int.parse(ageStr);
+  print('Мені $age ${getAgeSuffix(age)}');
 }
