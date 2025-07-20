040435730bfa6f8484a4741fabc5dd87bd97d9ff
Ivan Shlyapin <shlyapin@gmail.com>
2025-07-20 17:09:18 +0300

    feat(DART-11): Task 7 done

--- lib/homeworks/lesson_11/hw_streams.dart
+++ lib/homeworks/lesson_11/hw_streams.dart
@@ -1,5 +1,8 @@
 Stream<int> numberStream = Stream.fromIterable([1, 2, 3, 4, 5]);
 
+Stream<int> countdownStream() =>
+    Stream.periodic(Duration(seconds: 1), (x) => x + 1).take(10);
+
 Future<void> useAwaitFor() async {
   await for (int num in numberStream) {
     print('await for: $num');
@@ -15,4 +18,8 @@ void useListen() {
 void main() async {
   await useAwaitFor();
   useListen();
+
+  await for (var i in countdownStream()) {
+print('$i...');
+  }
 }
